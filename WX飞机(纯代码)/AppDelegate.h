33bdb1b6494335258cfe5b214feb48196cc6f75a
LockLight <Locklightz@gmail.com>
2016-12-25 10:46:42 +0800

    Initial Commit

---
+++ WX飞机(纯代码)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WX飞机(纯代码)
+//
+//  Created by locklight on 16/12/25.
+//  Copyright © 2016年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WX飞机(纯代码)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WX飞机(纯代码)
+//
+//  Created by locklight on 16/12/25.
+//  Copyright © 2016年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
